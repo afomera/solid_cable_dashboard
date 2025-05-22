07df7c6a37d6ff7b4f544b0959e3ef2c65d1a654
Andrea Fomera <afomera@hey.com>
2025-05-22 15:42:01 -0600

    lint

--- lib/solid_cable_dashboard/engine.rb
+++ lib/solid_cable_dashboard/engine.rb
@@ -11,19 +11,19 @@ module SolidCableDashboard
         solid_cable_dashboard/application.css
       ]
     end
-    
+
     initializer "solid_cable_dashboard.instrumentation" do
       config.after_initialize do
         SolidCableDashboard::Instrumentation.install
       end
     end
-    
+
     initializer "solid_cable_dashboard.pagy" do
       # Include Pagy modules in application controller and views
       ActiveSupport.on_load(:action_controller) do
         include Pagy::Backend
       end
-      
+
       ActiveSupport.on_load(:action_view) do
         include Pagy::Frontend
       end
